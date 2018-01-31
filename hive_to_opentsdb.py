3806d14d7e37794eb5687f62e9a3550bfc7c5d68
Alex Woolford <alex@woolford.io>
2018-01-31 11:32:33 -0700

    add all tags at once

--- hive_to_opentsdb.py
+++ hive_to_opentsdb.py
@@ -8,6 +8,7 @@ import configparser
 import potsdb
 import datetime
 import delorean
+import sys
 
 
 class HiveOpenTSDB:
@@ -40,14 +41,35 @@ class HiveOpenTSDB:
 
     def load_hive_to_opentsdb(self):
         table = Table(self.table, MetaData(bind=self.engine), autoload=True)
-        for value_column in self.value_columns:
-            for tag_column in self.tag_columns:
-                query = select([value_column, self.timestamp_column, tag_column], from_obj=table)
-                rows = self.session.execute(query).fetchall()
-                for row in rows:
-                    epoch_millis = self.get_epoch_millis(row[1])
-                    metrics_send_command = '''self.metrics.send("{0}", {1}, timestamp={2}, {3}="{4}")'''.format(value_column, row[0], epoch_millis, tag_column, row[2])
+        all_columns = self.value_columns + self.tag_columns + [self.timestamp_column]
+        query = select(all_columns, from_obj=table)
+        rows = self.session.execute(query).fetchall()
+
+        for row in rows:
+            try:
+                row_dict = dict(zip(all_columns, row))
+                epoch_millis = self.get_epoch_millis(row_dict.get(self.timestamp_column))
+                for value_column in self.value_columns:
+                    tags_dict = dict()
+                    for tag_column in self.tag_columns:
+                        tags_dict[tag_column] = row_dict[tag_column]
+
+                    tags_string = ", ".join(['''{0}="{1}"'''.format(key, value) for key, value in tags_dict.items()])
+
+                    if not row_dict.get(value_column):
+                        row_dict[value_column] = 0
+
+                    metrics_send_command = '''self.metrics.send("{0}", {1}, timestamp={2}, {3})'''.format(value_column,
+                                                                                                          row_dict.get(value_column),
+                                                                                                          epoch_millis,
+                                                                                                          tags_string)
+                    self.metrics.send("totalcapacitygb", 8575.0, timestamp=1.493728796e+12, tier="0", hostname="den2s1pure0100", vendor="pure", location="den2")
+
                     eval(metrics_send_command)
+            except:
+                # TODO: write error records somewhere
+                print sys.exc_info()
+
         self.metrics.wait()
 
 
